0d0657e63c0445c0347fb97ed228f6eaf7e20ddd
cewr <tmp.chenwei@ushareit.com>
2024-11-15 22:36:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>header</header>
+
+  <main>
+    main
+    <div class="wrapper">wrapper</div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
